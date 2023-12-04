b53c426269c1805ebd43de18c740bc3720914b53
mcatal12 <mcatal12@villanova.edu>
2023-12-04 16:18:26 -0500

    removing the error

--- Trivia/App.js
+++ Trivia/App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
